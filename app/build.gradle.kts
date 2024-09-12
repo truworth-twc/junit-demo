543436558ac17067c1532abe75b3049c99ead13b
vishnu-vk0104 <vishnu.kumar@truworthwellness.com>
2024-09-12 11:14:57 +0530

    Release version change for pom and jitpack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ afterEvaluate{
                 from(components.findByName("release")!!)
                 groupId = "com.github.truworth"
                 artifactId = "junit-aar-release"
-                version = "1.0.6"
+                version = "1.0.7"
             }
         }
         repositories {
