b281205df54da7b0af4d8cfde0411e2a23dacf2e
vishnu-vk0104 <vishnu.kumar@truworthwellness.com>
2024-09-12 16:59:37 +0530

    version 1.0.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ afterEvaluate{
                 from(components.findByName("release")!!)
                 groupId = "com.github.truworth"
                 artifactId = "junit-aar-release"
-                version = "1.0.9"
+                version = "1.0.10"
             }
         }
         repositories {
